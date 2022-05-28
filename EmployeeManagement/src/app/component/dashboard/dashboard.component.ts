47536fec1fbad0f5e4b45cd734940b382064d297
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-05-29 00:43:54 +0530

    Create CRUD components

--- EmployeeManagement/src/app/component/dashboard/dashboard.component.ts
+++ EmployeeManagement/src/app/component/dashboard/dashboard.component.ts
@@ -18,7 +18,9 @@ export class DashboardComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.getAllEmployee();
     this.empDetail = this.formBuilder.group({
+      id: [''],
       name: [''],
       email: [''],
       address: [''],
@@ -41,6 +43,7 @@ export class DashboardComponent implements OnInit {
     this.empService.addEmployee(this.empObj).subscribe(
       (res) => {
         console.log(res);
+        this.getAllEmployee();
       },
       (err) => {
         console.log(err);
@@ -49,10 +52,56 @@ export class DashboardComponent implements OnInit {
   }
 
   getAllEmployee() {
-    this.empService.getAllEmployee().subscribe(res => {
-      this.empList = res;
-    }, err => {
-      console.log("Error While Fletching Data..!")
-    })
+    this.empService.getAllEmployee().subscribe(
+      (res) => {
+        this.empList = res;
+      },
+      (err) => {
+        console.log('Error While Fletching Data..!');
+      }
+    );
+  }
+
+  editEmployee(emp: Employee) {
+    this.empDetail.controls['id'].setValue(emp.id);
+    this.empDetail.controls['name'].setValue(emp.name);
+    this.empDetail.controls['email'].setValue(emp.email);
+    this.empDetail.controls['address'].setValue(emp.address);
+    this.empDetail.controls['phone'].setValue(emp.phone);
+    this.empDetail.controls['designation'].setValue(emp.designation);
+    this.empDetail.controls['salary'].setValue(emp.salary);
+  }
+
+  updateEmployee() {
+    this.empObj.id = this.empDetail.value.id;
+    this.empObj.name = this.empDetail.value.name;
+    this.empObj.email = this.empDetail.value.email;
+    this.empObj.address = this.empDetail.value.address;
+    this.empObj.phone = this.empDetail.value.phone;
+    this.empObj.designation = this.empDetail.value.designation;
+    this.empObj.salary = this.empDetail.value.salary;
+
+    this.empService.updateEmployee(this.empObj).subscribe(
+      (res) => {
+        console.log(res);
+        this.getAllEmployee();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  deleteEmployee(emp: Employee) {
+    this.empService.deleteEmployee(emp).subscribe(
+      (res) => {
+        console.log(res);
+        alert('Employee deleted successfully');
+        this.getAllEmployee();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 }
