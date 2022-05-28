8a593966e38d8a86bb7eff063da59d992400fa6d
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-05-29 00:43:26 +0530

    Create delete , update and add employee http req

--- EmployeeManagement/src/app/services/employee.service.ts
+++ EmployeeManagement/src/app/services/employee.service.ts
@@ -9,10 +9,14 @@ import { Employee } from '../model/employee';
 export class EmployeeService {
   addEmpURL: string;
   getEmpURL: string;
+  updateEmpURL: string;
+  deleteEmpUrl: string;
 
   constructor(private http: HttpClient) {
     this.addEmpURL = 'http://llocalhost:4200/emp/addEmployee';
     this.getEmpURL = 'http://llocalhost:4200/emp/getAll';
+    this.updateEmpURL = 'http://llocalhost:4200/emp/updateEmployee';
+    this.deleteEmpUrl = 'http://localhost:9091/emp/deleteEmployeeById';
   }
   addEmployee(emp: Employee): Observable<Employee> {
     return this.http.post<Employee>(this.addEmpURL, emp);
@@ -20,5 +24,12 @@ export class EmployeeService {
 
   getAllEmployee(): Observable<Employee[]> {
     return this.http.get<Employee[]>(this.getEmpURL);
+  }
+
+  updateEmployee(emp: Employee): Observable<Employee> {
+    return this.http.post<Employee>(this.updateEmpURL, emp);
+  }
+  deleteEmployee(emp: Employee): Observable<Employee> {
+    return this.http.delete<Employee>(this.deleteEmpUrl + '/' + emp.id);
   }
 }
