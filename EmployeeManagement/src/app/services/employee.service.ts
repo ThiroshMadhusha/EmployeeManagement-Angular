2c0e2ec82359bc0733c347560f8d7dbc2b2223a8
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-05-28 19:48:00 +0530

    export employee class get employee details

--- EmployeeManagement/src/app/services/employee.service.ts
+++ EmployeeManagement/src/app/services/employee.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from '../model/employee';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  constructor(private http: HttpClient) {}
+  addEmpURL: string;
+  getEmpURL: string;
+
+  constructor(private http: HttpClient) {
+    this.addEmpURL = 'http://llocalhost:4200/emp/addEmployee';
+    this.getEmpURL = 'http://llocalhost:4200/emp/getAll';
+  }
+  addEmployee(emp: Employee): Observable<Employee> {
+    return this.http.post<Employee>(this.addEmpURL, emp);
+  }
+
+  getAllEmployee(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.getEmpURL);
+  }
 }
