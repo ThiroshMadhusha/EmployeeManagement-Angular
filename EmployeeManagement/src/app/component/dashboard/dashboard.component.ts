9a0f9dce962daf063330c2756f9419a49d707324
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-05-28 19:25:51 +0530

    create form builder group values

--- EmployeeManagement/src/app/component/dashboard/dashboard.component.ts
+++ EmployeeManagement/src/app/component/dashboard/dashboard.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  empDetail!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.empDetail = this.formBuilder.group({
+      name: [''],
+      email: [''],
+      address: [''],
+      phone: [''],
+      designation: [''],
+      salary: [''],
+    });
   }
 
+  addEmployee() {
+    console.log(this.empDetail);
+  }
 }
