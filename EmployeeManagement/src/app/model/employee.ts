e34f9d120dbd013b21fdaea538390561a759ca8e
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-05-28 19:26:41 +0530

    create employee model

--- EmployeeManagement/src/app/model/employee.ts
+++ EmployeeManagement/src/app/model/employee.ts
@@ -1,2 +1,9 @@
 export class Employee {
+  id: number = 0;
+  name: string = '';
+  email: string = '';
+  address: string = '';
+  phone: number = 0;
+  designation: string = '';
+  salary: number = 0;
 }
