c6140484f705c55829fad9bdf6a38ae65e36262a
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-05-28 19:26:24 +0530

    ad client http to server.ts file

--- EmployeeManagement/src/app/services/employee.service.ts
+++ EmployeeManagement/src/app/services/employee.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 }
