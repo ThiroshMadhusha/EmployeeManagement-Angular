7728df7c84353ef44f9fd6aac0b45d03cdbc9d46
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-05-28 19:47:32 +0530

    create Get all employee details

--- EmployeeManagement/src/app/component/dashboard/dashboard.component.ts
+++ EmployeeManagement/src/app/component/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { Employee } from 'src/app/model/employee';
+import { EmployeeService } from 'src/app/services/employee.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,7 +9,13 @@ import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 })
 export class DashboardComponent implements OnInit {
   empDetail!: FormGroup;
-  constructor(private formBuilder: FormBuilder) {}
+  empObj: Employee = new Employee();
+  empList: Employee[] = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private empService: EmployeeService
+  ) {}
 
   ngOnInit(): void {
     this.empDetail = this.formBuilder.group({
@@ -22,5 +30,29 @@ export class DashboardComponent implements OnInit {
 
   addEmployee() {
     console.log(this.empDetail);
+    this.empObj.id = this.empDetail.value.id;
+    this.empObj.name = this.empDetail.value.name;
+    this.empObj.email = this.empDetail.value.email;
+    this.empObj.address = this.empDetail.value.address;
+    this.empObj.phone = this.empDetail.value.phone;
+    this.empObj.designation = this.empDetail.value.designation;
+    this.empObj.salary = this.empDetail.value.salary;
+
+    this.empService.addEmployee(this.empObj).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  getAllEmployee() {
+    this.empService.getAllEmployee().subscribe(res => {
+      this.empList = res;
+    }, err => {
+      console.log("Error While Fletching Data..!")
+    })
   }
 }
